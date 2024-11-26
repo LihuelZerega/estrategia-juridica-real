550c4d4419b4e93870117dfadef7446cad55fe01
LihuelZerega <zeregalihuel@gmail.com>
2024-11-26 16:35:49 -0300

    servicios

--- src/components/servicios/hero-section/index.tsx
+++ src/components/servicios/hero-section/index.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import HeroBackground from "@/images/HeroServicios.png";
-import HeroDivideLine from "@/images/HeroDivideLine.png";
 import { motion } from "framer-motion";
-import { FaArrowRight } from "react-icons/fa";
 
 function Index() {
   return (
