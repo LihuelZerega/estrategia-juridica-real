c82414274046dc472ed53ce6a6b0cd7034ac2464
LihuelZerega <zeregalihuel@gmail.com>
2026-03-13 14:53:33 -0300

    fix: maps direccion

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <Navbar />
       <ContactForm />
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.600323628842!2d-99.16604819999999!3d19.4296674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff351188ed17%3A0x18ed2f8f96e5a2a8!2zQy4gUsOtbyBMZXJtYSA5NCwgQ3VhdWh0w6ltb2MsIDA2NTAwIENpdWRhZCBkZSBNw6l4aWNvLCBDRE1YLCBNw6l4aWNv!5e0!3m2!1ses-419!2sar!4v1736805805469!5m2!1ses-419!2sar"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.3407403598812!2d-99.16677626825211!3d19.426163539716104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff367f211b93%3A0x1f39cbe4591dda8e!2scorporativo%20amberes!5e0!3m2!1ses-419!2sar!4v1773424323626!5m2!1ses-419!2sar"
         width="1920"
         height="230"
         loading="lazy"
